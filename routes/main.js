e973632e595fc9694a3187a0e75677be31576daf
acyeh <allanyeh01@yahoo.com>
2016-11-02 11:27:59 -0700

    Merging with trung's change

--- routes/main.js
+++ routes/main.js
@@ -2,9 +2,27 @@
 /*
  * GET main page.
  */
-
+var data =require("../userData.json");
+var dict={}
 
 exports.view = function(req, res){
+  var nameToShow	=	req.params.uName;	
+
+  for(var users in data){
+  	var user = data[users][0];
+  	console.log("user is");
+  	console.log(user);
+  	console.log("user categories is")
+  	console.log(user.categories)
+  	for(var category in user.categories){
+  		console.log(category);
+  	}
+  }
+
+
+
+  console.log("dict is")
+//  console.log(dict)
+
   res.render('main');
 };
-
