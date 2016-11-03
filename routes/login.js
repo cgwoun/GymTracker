03dfa1d1923d8c4ad930396d4aeca212c66796bf
Chang Gwoun <cgwoun@ucsd.edu>
2016-11-03 16:50:39 -0700

    fixed login bug

--- routes/login.js
+++ routes/login.js
@@ -5,9 +5,9 @@ var data = require("../userData.json");
  */
 exports.view = function(req, res){
 
-	console.log("from login");
-  console.log(req.params);
-  console.log(req.query)	
+  // console.log("from login");
+  // console.log(req.params);
+  // console.log(req.query)	
   res.render('login',{});
 };
 
