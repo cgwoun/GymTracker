cceb8097b83574af05ddabb4e219d44089f78f72
Trung Do <dotrantrung@gmail.com>
2016-11-03 23:00:39 -0700

    try to fix adding data

--- public/javascript/jquery-main.js
+++ public/javascript/jquery-main.js
@@ -8,7 +8,9 @@ $(document).ready(function()
 	$("#addCategory").click(addACategory);
 	$(".addActivity").click(addAnActivity);
 	$(".activity").click(changedActivity);
-	$("#graph-button").click(addtoGraph);
+	// $("#graph-button").click(addtoGraph);
+	$("#addData-button").click(addData);
+
 	var array=$("#array").text();
 	var parsedArray=parseToArray(array);
 	renderTable(parsedArray);
@@ -65,17 +67,17 @@ function changedActivity(event)
 	 currentActivity=whoCalledMe;
 }
 
-function addtoGraph(event)
-{
-	if(currentActivity=="")
-	 {
-	 	alert("This button will let you add to the graph for this activity!");
-	 }
-	 else
-	 {
-	 	alert("This button will let you add to the graph for activity "+currentActivity);
-	 }
-}
+	// function addtoGraph(event)
+	// {
+	// 	if(currentActivity=="")
+	// 	 {
+	// 	 	alert("This button will let you add to the graph for this activity!");
+	// 	 }
+	// 	 else
+	// 	 {
+	// 	 	alert("This button will let you add to the graph for activity "+currentActivity);
+	// 	 }
+	// }
 function renderTable(array){
 	var iter=1;
 	console.log("in renderTable "+array)
@@ -106,5 +108,43 @@ function createLabelArray(array){
 		iter++;
 	}
 	return labelArray
+}
+function addData(event){
+	var value = $("#numRep-Textfield").val();
+	console.log(value);
+	var pathArray = window.location.pathname.split( '/' );
+	
+
+	var userName = pathArray[2];
+	var catName = pathArray[3];
+  //var catName = req.query.categ;
+   	pathArray[4].replace(/%20/g, " ");
+
+  	var actName = pathArray[4];
+	console.log(userName);
+	console.log(catName);
+	console.log(actName);
+  // console.log(userName);
+  // console.log(categ);
+  //console.log(newActivity);
+   var aNumber = value;
+
+  //  	var data = require("../userData.json");
+
+  // for(i=0; i<data.users.length; i++){
+  // 	if(data.users[i].id === userName){
+  // 		for(j=0; j<data.users[i].categories.length; j++){
+  // 			if(data.users[i].categories[j].categoryName === catName){
+  // 				for(k =0; k<data.users[i].categories[j].activities.length; k++){
+		// 			if (activities[k]=== actName)
+  // 					var temp = data.users[i].categories[j].activities[i].data;
+  // 					temp.push(aNumber);
+  // 					console.log(temp);
+  // 					break;
+  // 				}
+  // 			}
+  // 		}
+  // 	}
+  // }
 }
 
