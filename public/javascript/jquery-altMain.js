ccd50c9ee9c1b143622a8e53eaf9a3d0fb67dda6
acyeh <allanyeh01@yahoo.com>
2016-11-20 15:48:45 -0800

    Made changes to main2, added how this graph works button in main2 but it's not connected

--- public/javascript/jquery-altMain.js
+++ public/javascript/jquery-altMain.js
@@ -127,13 +127,12 @@ function renderTable(array,labelArray){
 
 	for(element=0; element < array.length; element++){
 
-		console.log("label is "+ labelArray[element]);
-		console.log("element is "+ array[element]);
 		label=String(labelArray[element])
 		console.log("Label is "+label);
 		console.log("Type is  " +typeof(label));
 		//var html="<tr><td style='cursor:pointer;' onclick='setDate("+String(label)+")'>"+labelArray[element]+"</td><td>"+array[element]+"</td></tr>";
-		var html="<tr><td style='cursor:pointer;' onclick='setDate("+'"'+labelArray[element]+'"'+")'>"+labelArray[element]+"</td><td>"+array[element]+"</td></tr>";
+		var html="<tr><td id='"+labelArray[element]+"' style='cursor:pointer;' onclick='setDate("+'"'+labelArray[element]+'"'+")'>"+labelArray[element]+"</td><td>"+array[element]+"</td></tr>";
+
 
 		// var html = "<tr><td>what up</td><td>"+array[element]+"</td></tr>";
 
@@ -141,7 +140,7 @@ function renderTable(array,labelArray){
 	// for(element in array){
 	// 	//console.log("element is "+ array[element]);
 	// 	label=labelArray[element].slice(-1);
-	// 	var html="<tr><td style='cursor:pointer;' onclick='setDate("+label+")'>"+label+"</td><td>"+array[element]+"</td></tr>";
+	//  	var html="<tr><td style='cursor:pointer;' onclick='setDate("+label+")'>"+label+"</td><td>"+array[element]+"</td></tr>";
 
 		//console.log(html);
 		$("#tableBody").append (html);
@@ -150,12 +149,32 @@ function renderTable(array,labelArray){
 	}
 
 }
+var oldColor;
+var oldHighlighted;
+var alreadyHighlight=false;
 function setDate(label){
-    var whoCalledMe=$(this);
- //   console.log(whoCalledMe);
- 	text=whoCalledMe.text();
-    console.log("whoCalled me text"+ text);
-  //  console.log($("#editDay"));
+    myString=String(label);
+ 	//idMe=$(myString);
+ 	idMe=document.getElementById(myString);
+ 	console.log(idMe);
+ 	//oldColor=idMe.style.backgroundColor;
+ 	
+
+ 	if(alreadyHighlight==false){
+ 		oldColor=idMe.style.backgroundColor;
+ 		oldHighlighted=label;
+ 		idMe.style.backgroundColor="yellow";
+ 		alreadyHighlight=true;
+ 	}
+ 	else{
+ 		//Set the old highlighted to it's original color
+ 		document.getElementById(oldHighlighted).style.backgroundColor=oldColor;
+ 		oldColor=idMe.style.backgroundColor;
+ 		oldHighlighted=label;
+ 		idMe.style.backgroundColor="yellow";
+
+ 	}
+
     $("#editDay").val(label);
 }
 function parseToArray(htmlText)
